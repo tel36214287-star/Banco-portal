92389fb3a7760e8be4793bcd22836dfd7b0be8f3
D-Renato <tel36214287@gmail.com>
2025-11-30 10:55:22 -0300

    Create index.js

---
+++ publics/index.js
@@ -0,0 +1,58 @@
+{
+  "clientes": [
+    {
+      "id": 1,
+      "nome": "Cliente Padrão",
+      "cpf": "123.456.789-00",
+      "senha": "1234",
+      "saldo": 12450.75,
+      "extrato": [
+        {
+          "data": "2025-11-28",
+          "descricao": "Depósito",
+          "valor": 1000.00,
+          "tipo": "Crédito"
+        },
+        {
+          "data": "2025-11-27",
+          "descricao": "Pagamento Conta de Luz",
+          "valor": 220.50,
+          "tipo": "Débito"
+        },
+        {
+          "data": "2025-11-26",
+          "descricao": "Transferência Recebida",
+          "valor": 500.00,
+          "tipo": "Crédito"
+        },
+        {
+          "data": "2025-11-25",
+          "descricao": "Compra Mercado",
+          "valor": 150.25,
+          "tipo": "Débito"
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "nome": "Guilherme Jahu",
+      "cpf": "987.654.321-00",
+      "senha": "abcd",
+      "saldo": 8420.50,
+      "extrato": [
+        {
+          "data": "2025-11-28",
+          "descricao": "Depósito",
+          "valor": 800.00,
+          "tipo": "Crédito"
+        },
+        {
+          "data": "2025-11-27",
+          "descricao": "Pagamento Internet",
+          "valor": 120.00,
+          "tipo": "Débito"
+        }
+      ]
+    }
+  ]
+}
